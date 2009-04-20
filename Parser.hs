917e947b4b0adc8498a387b22c915685bce358ec
Carl Jackson <carl@gootproductions.com>
2009-04-19 22:23:33 -0400

    Changed display code (ch 3)

--- Parser.hs
+++ Parser.hs
@@ -16,7 +16,22 @@ data LispVal = Atom String
              | String String
              | Bool Bool
              | Char Char
-               deriving (Show)
+instance Show LispVal where show = showVal
+
+-- Display LispVals
+showVal :: LispVal -> String
+showVal (Atom name) = name
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+showVal (Number contents) = show contents
+-- Need one for Float
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+-- Need one for Char
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
 
 -- Supporting Functions
 
