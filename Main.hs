325c73b8689c2b9c7882c1aaf8e9c0ca3e528167
Christian Anderson <christian@christian.home>
2009-05-02 22:01:06 -0400

    Completed Chapter 6.

--- Main.hs
+++ Main.hs
@@ -3,12 +3,39 @@ import System.Environment
 import Parser
 import Eval
 import Datatypes
+import IO hiding (try)
 import Monad(liftM)
+
+--REPL components.  Do we want a separate file?
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ parseScheme expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+     then return ()
+     else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
 
 -- This file contains the main loop of the interpreter
 
 main :: IO ()
 main = do
   args <- getArgs
-  evaled <- return $ liftM show $ parseScheme (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ args !! 0
+    otherwise -> putStrLn "Program takes only 0 or 1 arguments."
